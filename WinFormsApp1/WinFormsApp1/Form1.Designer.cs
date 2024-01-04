4438acf9ea1dd9ce57f81f9ecc18d77909a2045b
Dinn0o0 <113049615+Dinn0o0@users.noreply.github.com>
2024-01-05 00:04:51 +0300

    Add files via upload

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            newgame = new Button();
+            SuspendLayout();
+            // 
+            // newgame
+            // 
+            newgame.Location = new Point(12, 12);
+            newgame.Name = "newgame";
+            newgame.Size = new Size(90, 23);
+            newgame.TabIndex = 0;
+            newgame.Text = "Новая игра";
+            newgame.UseVisualStyleBackColor = true;
+            newgame.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(396, 414);
+            Controls.Add(newgame);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button newgame;
+    }
+}
