1f1a581d6c0e662bfc4acb8ff26982f1b4dd7717
Daria-Look <105943267+Daria-Look@users.noreply.github.com>
2024-01-09 21:12:12 +0300

    а это куда

--- WinFormsApp1/WinFormsApp1/obj/Release/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ WinFormsApp1/WinFormsApp1/obj/Release/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2423318284fbaed4646c10902232937e21d5047d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99ec8f8f0239fc0a4614928b085c2fdf6a9ed001")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
